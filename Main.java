cb600af9b5ad6471bd32de4561846649aec3bd26
laceda <lacedajonalyn06@gmail.com>
2025-08-18 15:30:51 +0800

    Activity #1 Basic Math with Condition

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void checkGrade(int grade) {
+        if (grade >= 75) {
+            System.out.println("Your grade is " + grade + ". Congratulations! You Passed.");
+        } else {
+            System.out.println("Your grade is " + grade + ". Sorry, you Failed.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your grade: ");
+        int grade = scanner.nextInt();
+        checkGrade(grade);
+    }
+}
